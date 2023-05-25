0b8c136fabb0d1d17962488cd5f69a5b18b30df9
Solomon Hsu <solnone@gmail.com>
2023-05-25 18:37:51 +0800

    Update TestJson.py

--- TestJson.py
+++ TestJson.py
@@ -13,6 +13,29 @@ from dotenv import load_dotenv
 import os
 import json
 
+llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+k = 0 # keep the last {k} interactions in memory
+memory = ConversationBufferWindowMemory(k = k, return_messages = True)    
+        
+def create_chat(state):
+    system_message = f"""
+`no explanations`
+`no prompt`
+You are a wise steward, The current environment is represented in JSON: 
+{{{json.dumps(state)}}}
+Please check the environment to update JSON.
+Format your response as a JSON object with "msg", "light", "door" keys.
+"""        
+    prompt = ChatPromptTemplate.from_messages([
+        SystemMessagePromptTemplate.from_template(system_message),
+        MessagesPlaceholder(variable_name = "history"),
+        HumanMessagePromptTemplate.from_template("{input}")
+    ])
+    conversation = ConversationChain(
+        memory = memory, prompt = prompt, llm = llm, verbose = True
+    )
+    return conversation
+        
 def main():
     load_dotenv()
 
@@ -22,47 +45,21 @@ def main():
         exit(1)
     else:
         print("OPENAI_API_KEY is set")
-        
-    state = {"light": 0, "door_key": 0, "door": 0, "msg": "The light is off, the door is closed"}
+
+    state = {
+        "light": 0,
+        "door": 0, 
+        "msg": "The light is off, the door is closed"
+    }
     
-    key_password = "0525"
-
-    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
-    k = 2 # keep the last {k} interactions in memory
-    memory = ConversationBufferWindowMemory( k=k, return_messages=True)    
-    # memory.save_context({"Human": "Display state"}, {"AI": """{"light": 0, "door_key": 0, "door": 0, "msg": "The light is off, the door is closed."}""" })
-
     while True:
         print(json.dumps(state))
         user_input = input("> ")
-        if len(user_input) == 4 and user_input.find(key_password) != -1:
-            state["door_key"] = 1
-            state["msg"] = "You got the key."
-            memory.save_context({"Human": "Got the house key."}, {"AI": json.dumps(state) })
-            continue
-            
-        system_message = f"""
-`no explanations`
-`no prompt`
-The current environment is represented in JSON: 
-{{{json.dumps(state)}}}
-Format your response as a JSON object with "msg", "light", "door" and "door_key" keys.
-A "door_key" is required to open the door in JSON object.
-"""
-#  Make your response as short as possible.
-
-        prompt = ChatPromptTemplate.from_messages([
-            SystemMessagePromptTemplate.from_template(system_message),
-            MessagesPlaceholder(variable_name = "history"),
-            HumanMessagePromptTemplate.from_template("{input}")
-        ])
-        
-        conversation = ConversationChain(
-            memory = memory, prompt = prompt, llm = llm, verbose = True
-        )
+        conversation = create_chat(state)  
         response = conversation.predict(input=user_input)
         try:
             state = json.loads(response[response.find("{"):response.find("}") + 1])
+
             # state = json.loads(response)
         except Exception as e:
             print(e)
